cf551664e489817cd4f936370346add1d362892b
Charly <carlostonche86@gmail.com>
2025-06-05 17:39:37 -0700

    db remota

--- config/db.php
+++ config/db.php
@@ -1,15 +1,15 @@
 <?php
 // Configuración de la base de datos del hosting
-/*define('DB_HOST', 'srv529.hstgr.io');
+define('DB_HOST', 'srv529.hstgr.io');
 define('DB_USER', 'u827377324_admin');
 define('DB_PASS', '!1CmmHkk!');
-define('DB_NAME', 'u827377324_tiendakiky');*/
-
+define('DB_NAME', 'u827377324_tiendakiky');
+/*
 //info de base de datos local
 define('DB_HOST', 'localhost');
 define('DB_USER', 'admin');
 define('DB_PASS', 'admin');
-define('DB_NAME', 'tienda_online');
+define('DB_NAME', 'tienda_online');*/
 
 // Crear conexión
 function conectarDB() {

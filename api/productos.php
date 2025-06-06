0e490f4f6290fecab77cef54e65313fa3b6b40b1
Charly <carlostonche86@gmail.com>
2025-06-05 19:04:08 -0700

    cambio

--- api/productos.php
+++ api/productos.php
@@ -5,7 +5,6 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type');
 
 
-
 require_once __DIR__ . '/../config/db.php';
 require_once __DIR__ . '/../includes/auth.php';
 

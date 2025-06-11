a92f3a5fc9d90d26b3ecc1e0e1757364e6ec8fc5
Charly <carlostonche86@gmail.com>
2025-06-10 21:23:28 -0700

    cosilla

--- api/pedidos.php
+++ api/pedidos.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET');
+header('Access-Control-Allow-Methods: POST, GET,  PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type');
 
 require_once __DIR__ . '/../config/db.php';
